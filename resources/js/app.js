22663722cf53c30e96a4a7ef7fe35f6bd9504967
Sunwarul Islam <sanoarulslm@gmail.com>
2025-01-09 18:29:33 +0600

    Integrating prime-vue component library

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,21 @@
-import '../css/app.css';
 import './bootstrap';
+
+import '../css/app.scss';
+import 'primeicons/primeicons.css'
+
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+import PrimeVue from 'primevue/config';
+
+import ConfirmationService from 'primevue/confirmationservice';
+import DialogService from 'primevue/dialogservice';
+import ToastService from 'primevue/toastservice';
+import Noir from './primevue/Noir';
+import AppState from './primevue/AppState';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +30,15 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(ConfirmationService)
+            .use(ToastService)
+            .use(DialogService)
+            .use(AppState)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Noir
+                }
+            })
             .mount(el);
     },
     progress: {
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+// PrimeVue related imports
+import Components from 'unplugin-vue-components/vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+
 export default defineConfig({
+    server: { // Open to load fonts from node_modules directory
+        fs: {
+            allow: ['node_modules', 'resources', 'vendor'],
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         vue({
@@ -15,6 +24,11 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }), // primevue related starts
+        Components({
+            resolvers: [
+                PrimeVueResolver()
+            ]
         }),
     ],
 });
