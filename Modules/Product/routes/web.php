db632a7b4f3535ff1617dab8c46535a9a6c9a43f
Sunwarul Islam <sanoarulslm@gmail.com>
2025-03-08 14:07:00 +0600

    fix: export issue fix due to the route discovery issue. fixed now.

--- Modules/Product/routes/web.php
+++ Modules/Product/routes/web.php
@@ -8,27 +8,28 @@ use Modules\Product\Http\Controllers\CategoryController;
 use Modules\Product\Http\Controllers\SubCategoryController;
 
 # AUTH & VERIFIED
+
 Route::middleware(['auth', 'verified'])->group(function () {
     // Categories
     Route::resource('categories', CategoryController::class);
-    Route::get('/categories/export', [CategoryController::class, 'export'])->name('categories.export');
-    Route::post('/categories/bulk-destroy', [CategoryController::class, 'bulkDestroy'])->name('categories.bulk-destroy');
+    Route::get('/categories/export/excel', [CategoryController::class, 'export'])->name('categories.export');
+    Route::post('/categories/destroy/bulk', [CategoryController::class, 'bulkDestroy'])->name('categories.bulk-destroy');
 
     // Sub-Categories
     Route::resource('sub-categories', SubCategoryController::class);
-    Route::get('/sub-categories/export', [SubCategoryController::class, 'export'])->name('sub_categories.export');
+    Route::get('/sub-categories/export/excel', [SubCategoryController::class, 'export'])->name('sub_categories.export');
 
     // Brands
     Route::resource('brands', BrandController::class);
-    Route::post('/brands/bulk-destroy', [BrandController::class, 'bulkDestroy'])->name('brands.bulk-destroy');
-    Route::get('/brands/export', [BrandController::class, 'export'])->name('brands.export');
+    Route::post('/brands/destroy/bulk', [BrandController::class, 'bulkDestroy'])->name('brands.bulk-destroy');
+    Route::get('/brands/export/excel', [BrandController::class, 'export'])->name('brands.export');
 
     // Tags
     Route::resource('tags', TagController::class);
-    Route::post('/tags/bulk-destroy', [TagController::class, 'bulkDestroy'])->name('tags.bulk-destroy');
-    Route::get('/tags/export', [TagController::class, 'export'])->name('tags.export');
+    Route::post('/tags/destroy/bulk', [TagController::class, 'bulkDestroy'])->name('tags.bulk-destroy');
+    Route::get('/tags/export/excel', [TagController::class, 'export'])->name('tags.export');
 
     // Colors
     Route::resource('colors', ColorController::class);
-    Route::post('/colors/bulk-destroy', [ColorController::class, 'bulkDestroy'])->name('colors.bulk-destroy');
+    Route::post('/colors/destroy/bulk', [ColorController::class, 'bulkDestroy'])->name('colors.bulk-destroy');
 });
